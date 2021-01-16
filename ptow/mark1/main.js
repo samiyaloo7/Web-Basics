94e51f49e8faf0b572c8512aa82141575e9eb6af
$amiyal : ) <samiyaloo7@samiyalization.github.com>
2021-01-16 09:25:16 +0530

    big_screen_complete

---
+++ ptow/mark1/main.js
@@ -0,0 +1,8 @@
+function cnglinkcolor (t) {
+    document.getElementById('target_'+t).src = "imgs/Arrow  (2).png";
+	document.getElementById('at'+t).style.color = "orange";
+}
+function oldlinkcolor (t) {
+    document.getElementById('target_'+t).src = "imgs/Arrow  (1).png";
+	document.getElementById('at'+t).style.color = "black";
+}
