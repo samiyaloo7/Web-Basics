0d9e024e5c25f2572d48c2a64e42dc9283e309d5
$amiyal : ) <samiyaloo7@samiyalization.github.com>
2021-01-20 09:17:25 +0530

    final update

--- ptow/mark1/main.js
+++ ptow/mark1/main.js
@@ -1,3 +1,6 @@
+function sidelinkclick() {
+    document.getElementById('mob_panel').style.right= '-100vw';
+}
 function cnglinkcolor (t) {
     document.getElementById('target_'+t).src = "imgs/Arrow  (2).png";
 	document.getElementById('at'+t).style.color = "orange";
@@ -13,3 +16,4 @@ document.getElementById("cls_icon").onclick = function () {
 document.getElementById("menu_icon").onclick = function () {
     document.getElementById("mob_panel").style.right = "0";
 }
+
